328f432e5eb565ead9a9b77ccb1111b3ed86af88
RubP04 <rubenruben1521@gmail.com>
2025-01-18 12:47:36 -0500

    Create function that creates user accounts using Supabase authentication and returns access token

--- db.py
+++ db.py
@@ -7,3 +7,15 @@ load_dotenv()
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+def create_user(email, password):
+    response = supabase.auth.sign_up(
+        {
+            "email": email,
+            "password": password,
+        }
+    )
+
+    session = response.get("session")
+
+    return session.get("access_token")
