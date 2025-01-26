12a910acbc2a59efe859aabdc137dc5343ce2c3a
RubP04 <rubenruben1521@gmail.com>
2025-01-25 21:03:13 -0500

    Delete ugly image from home page

--- watchedit/src/Components/Home.jsx
+++ watchedit/src/Components/Home.jsx
@@ -8,9 +8,6 @@ const Home = () => {
         <h2>A selection of movies handpicked just for you...</h2>
         <h4>A simple and easy way to find movies tailored to your preferences. Click the reccomendations button to get started.</h4>
       </div>
-      <div className="image">
-        <img src="./main.png"></img>
-      </div>
     </div>
     <div className="gif-container">
       <div>
