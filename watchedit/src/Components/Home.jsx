a4f7a711986dda9c898ad560be2303a1ac3ac0fb
RubP04 <rubenruben1521@gmail.com>
2025-02-20 14:46:32 -0500

    Updated home page HTML to remove broken gif

--- watchedit/src/Components/Home.jsx
+++ watchedit/src/Components/Home.jsx
@@ -8,32 +8,6 @@ const Home = () => {
         <h2 style={{marginRight: 20}}>Find movies and create a watchlist that's right for</h2><h2 className="highlight">you...</h2>
       </div>
     </div>
-    <div className="gif-container">
-      <div>
-        <p>Movies recommended based on your preferences.</p>
-      </div>
-      <div style={{ height: 0, paddingBottom: 'calc(56.25%)', position: 'relative', width: '100%'}}>
-        <iframe
-          allow="autoplay; gyroscope;"
-          allowFullScreen
-          height="100%"
-          referrerPolicy="strict-origin"
-          src="https://www.kapwing.com/e/67958d08c63ad2b1bbb1fb1e"
-          style={{
-            border: 0,
-            height: '40%',
-            left: 0,
-            overflow: 'hidden',
-            position: 'absolute',
-            top: 0,
-            width: '80%',
-            margin: 0,
-          }}
-          title="Embedded content made on Kapwing"
-          width="100%"
-        ></iframe>
-        </div>
-      </div>
     </>
   )
 }
